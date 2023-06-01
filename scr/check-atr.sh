7d263763de83f7a4ccaf345675895ad214215e2b
Taichi-Ibi <t.ibi@estyle-inc.jp>
2023-06-02 00:27:50 +0900

    [mod] ATRトークンの抽出ロジックの変更、引数の変更

--- scr/check-atr.sh
+++ scr/check-atr.sh
@@ -2,15 +2,24 @@
 
 ################################################################################
 # 処理概要:      Tableau DesktopのATRトークンの状態をSlackチャンネルに通知する
-# コマンド:      curl -sf https://raw.githubusercontent.com/Taichi-Ibi/tableau-account-management/main/scr/check-atr.sh | sh -s {WEBHOOK_URL} {TABLEAU_VERSION}
+# コマンド:      curl -sf https://raw.githubusercontent.com/Taichi-Ibi/tableau-account-management/main/scr/check-atr.sh | sh -s {TABLEAU_VERSION} {WEBHOOK_URL}
 # 作者:          Phil (Taichi Ibi)
 # 作成日時:      2023-05-26
 ################################################################################
 
-HOSTNAME=`hostname`
-ATR=`"/Applications/Tableau Desktop "$2".app/Contents/MacOS/atrdiag" | sed -n "3, 8p"`
-MESSAGE=$HOSTNAME"\n"$ATR
-MESSAGE=$(echo "$MESSAGE" | tr -s '\n')
-echo "$MESSAGE"
-PAYLOAD="payload={\"text\": \"$MESSAGE\"}"
-curl -XPOST --data-urlencode "${PAYLOAD}" "$1" > /dev/null
+HOSTNAME=`hostname` # PC名を取得
+if [ $# = 0 ]; then
+    exit 1 # 引数がなかったらversion特定できないので終了
+else
+    ATR=`"/Applications/Tableau Desktop "$1".app/Contents/MacOS/atrdiag" | grep -m 2 TTL`
+    MESSAGE=$HOSTNAME"\n"$ATR
+    MESSAGE=$(echo "$MESSAGE" | tr -s '\n')
+    echo "$MESSAGE" # ATRトークンの期限を出力
+fi
+if [ $# = 1 ]; then
+    exit 0
+else # WEBHOOK_URLが指定された場合
+    PAYLOAD="payload={\"text\": \"$MESSAGE\"}"
+    curl -XPOST --data-urlencode "${PAYLOAD}" "$2" > /dev/null # slackに通知
+    exit 0
+fi
